762688a359a77ca1e3d259a68691da0d28b8c2e0
vlad <vvidavsky@gmail.com>
2020-03-30 00:16:48 +0300

    Creating empty requests list placeholder

---
+++ src/components/EmptyMonitorViewPlaceholder.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="empty-list-placeholder">REQUESTS LOG WILL APPEAR HERE</div>
+</template>
+
+<script>
+  export default {
+    name: 'EmptyMonitorViewPlaceholder'
+  }
+</script>
+
+<style lang="sass">
+    .empty-list-placeholder
+        height: 100%
+        width: 100%
+        display: flex
+        align-items: center
+        justify-content: center
+        font-size: 13px
+        color: #858585
+        font-weight: bold
+</style>
--- src/components/RequestsMonitor.vue
+++ src/components/RequestsMonitor.vue
@@ -1,32 +1,37 @@
 <template>
     <section class="requests-monitor">
         <h3>Requests Monitor</h3>
-        <ul class="requests-lists">
+        <ul class="requests-lists" v-if="requests_list.length">
             <li v-for="request in requests_list" :key="request.timeStamp" :class="{'ignored': !request.accepted}">
                 <span>{{request.initiator}}</span>
                 <span>{{request.requestId}}</span>
                 <span>{{getRowStatus(request)}}</span>
             </li>
         </ul>
+        <list-placeholder v-if="!requests_list.length"></list-placeholder>
     </section>
 </template>
 
 <script>
   import { mapState } from 'vuex'
-  import axios from "axios";
+  import axios from 'axios'
+  import ListPlaceholder from './EmptyMonitorViewPlaceholder'
 
   export default {
     name: 'RequestsMonitor',
-    data() {
+    components: {
+      ListPlaceholder
+    },
+    data () {
       return {
         requests_list: []
       }
     },
     created () {
-      this.initiateRequestsListener();
+      this.initiateRequestsListener()
     },
     computed: {
-      ...mapState(["ignored_urls"])
+      ...mapState(['ignored_urls'])
     },
     methods: {
       /**
@@ -35,34 +40,34 @@
        * @return {string}
        */
       getRowStatus: (request) => {
-        return !request.accepted ? "IGNORED" : request.accepted && request.done ? "SENT" : request.accepted ? "SENDING..." : "";
+        return !request.accepted ? 'IGNORED' : request.accepted && request.done ? 'SENT' : request.accepted ? 'SENDING...' : ''
       },
 
       /**
        * Function that initiates listener for browser http requests.
        */
-      initiateRequestsListener() {
-        const self = this;
+      initiateRequestsListener () {
+        const self = this
         browser.runtime.onMessage.addListener(function (msg) {
-          msg.accepted = !self.ignored_urls.includes(msg.initiator);
-          const alreadyExists = self.requests_list.some(url => url.requestId === msg.requestId);
+          msg.accepted = !self.ignored_urls.includes(msg.initiator)
+          const alreadyExists = self.requests_list.some(url => url.requestId === msg.requestId)
           if (!alreadyExists) {
-            self.requests_list.unshift(msg);
+            self.requests_list.unshift(msg)
           }
-          self.sendRequestDataToServer(msg);
-        });
+          self.sendRequestDataToServer(msg)
+        })
       },
 
       /**
        * This part sends requst data to server
        * @param msg - object that contains request data
        */
-      sendRequestDataToServer(msg) {
+      sendRequestDataToServer (msg) {
         if (msg.accepted) {
           axios.post('https://jsonplaceholder.typicode.com/posts', msg).then(resp => {
-            const foundRequest = this.requests_list.find(item => item.requestId === resp.data.requestId);
+            const foundRequest = this.requests_list.find(item => item.requestId === resp.data.requestId)
             if (foundRequest) {
-              foundRequest.done = true;
+              foundRequest.done = true
             }
           })
         }
@@ -77,8 +82,10 @@
         overflow: hidden
         padding: 10px 0 0 10px
         +setGrid(null, null, auto 1fr, 10px, "rows")
+
     .requests-lists
         overflow: auto
+
         li
             padding: 0 0 0 5px
             align-items: center
@@ -87,9 +94,11 @@
             border-bottom: solid 1px #7dd67d
             height: 30px
             +setGrid(1fr auto 80px, 5px, null, null, "columns")
+
             span
                 &:first-child
                     +ellipsis()
+
                 &:last-child
                     display: flex
                     justify-content: flex-end
